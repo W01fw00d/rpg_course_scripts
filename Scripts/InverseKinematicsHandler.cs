b3cd44d4f69919d13ea9b4064fa0c20160a16baf
W01fw00d <gabrm_91@hotmail.com>
2018-11-07 20:31:22 +0100

    Finish inverse kinematics handler

--- Scripts/InverseKinematicsHandler.cs
+++ Scripts/InverseKinematicsHandler.cs
@@ -6,7 +6,7 @@ public class InverseKinematicsHandler : MonoBehaviour {
 
     private Animator animator;
 
-    public Transform LeftFoot, rightFoot;
+    public Transform leftFoot, rightFoot;
 
     //  USED FOR MANUAL TESTING no se compila!!!
     #if UNITY_EDITOR
@@ -34,16 +34,92 @@ public class InverseKinematicsHandler : MonoBehaviour {
 
     #endif
 
-
-
     void Start () {
         animator = GetComponent<Animator>();
 
-        LeftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
+        leftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
         rightFoot = animator.GetBoneTransform(HumanBodyBones.RightFoot);
     }
 	
 	void Update () {
-		
-	}
+        // Me sirve para saber dónde está mirando el personaje
+        Ray ray = new Ray(
+            Camera.main.transform.position,
+            Camera.main.transform.forward
+        );
+
+        Debug.DrawRay(
+            Camera.main.transform.position,
+            Camera.main.transform.forward * 20.0f,
+            Color.cyan
+        );
+
+        RaycastHit leftHit;
+        RaycastHit rightHit;
+
+        // Obtener la posición en referencia al punto zero global
+        Vector3 lPos = leftFoot.TransformPoint(Vector3.zero);
+        Vector3 rPos = rightFoot.TransformPoint(Vector3.zero);
+
+        if (Physics.Raycast(lPos, -Vector3.up, out leftHit, 1.0f))
+        {
+            leftFootPosition = leftHit.point;
+            leftFootRotation = 
+                Quaternion.FromToRotation(transform.up, leftHit.normal) * transform.rotation;
+        }
+
+        Debug.DrawRay(
+            lPos,
+            Vector3.down,
+            Color.red
+        );
+
+
+        if (Physics.Raycast(rPos, -Vector3.up, out rightHit, 1.0f))
+        {
+            rightFootPosition = rightHit.point;
+            rightFootRotation =
+                Quaternion.FromToRotation(transform.up, rightHit.normal) * transform.rotation;
+        }
+
+        Debug.DrawRay(
+            rPos,
+            Vector3.down,
+            Color.green
+        );
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        leftFootWeight = animator.GetFloat("leftFoot");
+        rightFootWeight = animator.GetFloat("rightFoot");
+
+        // Position
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, leftFootWeight);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, rightFootWeight);
+
+        animator.SetIKPosition(
+            AvatarIKGoal.LeftFoot,
+            leftFootPosition + new Vector3(0, offsetY, 0)
+        );
+
+        animator.SetIKPosition(
+            AvatarIKGoal.RightFoot,
+            rightFootPosition + new Vector3(0, offsetY, 0)
+        );
+
+        // Rotation
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, leftFootWeight);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, rightFootWeight);
+
+        animator.SetIKRotation(
+            AvatarIKGoal.LeftFoot,
+            leftFootRotation
+        );
+
+        animator.SetIKRotation(
+            AvatarIKGoal.RightFoot,
+            rightFootRotation
+        );
+    }
 }
