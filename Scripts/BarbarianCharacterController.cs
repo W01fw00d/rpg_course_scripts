eb9760a0198c8342b000134f9d64c1fe221cfe50
W01fw00d <gabrm_91@hotmail.com>
2018-11-07 19:29:52 +0100

    Continue course, animations controller done and inverse kinematics is WIP

---
+++ Scripts/BarbarianCharacterController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarbarianCharacterController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float horizontal = 0.0f;
+    public float vertical = 0.0f;
+    public bool attack = false;
+    public bool jump = false;
+    public bool die = false; //muriendo, transición
+    public bool superattack = false;
+
+    public bool run = false;
+    public bool dead = false; //muerto, estado
+    public Vector3 moveDirection = Vector3.zero;
+
+    private Animator animator;
+
+    private const string
+        ANIM_SPEED = "speed",
+        ANIM_HORIZONTAL = "horizontal",
+        ANIM_VERTICAL = "vertical",
+        ANIM_JUMP = "jump",
+        ANIM_ATTACK = "attack",
+        ANIM_SUPER_ATTACK = "super-attack",
+        ANIM_DIE = "die",
+        ANIM_IDLE = "idle",
+        ANIM_RUN = "run";
+
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if (dead)
+        {
+            if (die)
+            {
+                animator.SetBool(ANIM_DIE, true);
+                die = false;
+            }
+
+            return;
+        }
+
+
+        if (!attack && Input.GetKeyDown(KeyCode.C))
+        {
+            attack = true;
+            animator.SetBool(ANIM_ATTACK, attack);
+        }
+
+        if (attack && Input.GetKeyUp(KeyCode.C))
+        {
+            attack = false;
+            animator.SetBool(ANIM_ATTACK, attack);
+        }
+
+        if (!superattack && Input.GetKeyDown(KeyCode.V))
+        {
+            superattack = true;
+            animator.SetBool(ANIM_SUPER_ATTACK, superattack);
+        }
+
+        if (superattack && Input.GetKeyUp(KeyCode.V))
+        {
+            superattack = false;
+            animator.SetBool(ANIM_SUPER_ATTACK, superattack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            run = false;
+        }
+
+        animator.SetBool(ANIM_RUN, run);
+
+
+        if (!jump && Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+            animator.SetBool(ANIM_JUMP, jump);
+        }
+
+        if (jump && Input.GetKeyUp(KeyCode.Space))
+        {
+            jump = false;
+            animator.SetBool(ANIM_JUMP, jump);
+        }
+
+        // Debug death
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            die = true;
+            dead = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        speed = new Vector2(horizontal, vertical).sqrMagnitude;
+
+        animator.SetFloat(ANIM_SPEED, speed);
+        animator.SetFloat(ANIM_HORIZONTAL, horizontal);
+        animator.SetFloat(ANIM_VERTICAL, vertical);
+    }
+}
---
+++ Scripts/InverseKinematicsHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseKinematicsHandler : MonoBehaviour {
+
+    private Animator animator;
+
+    public Transform LeftFoot, rightFoot;
+
+    //  USED FOR MANUAL TESTING no se compila!!!
+    #if UNITY_EDITOR
+
+    #region TESTING
+    public Transform leftIKTarget, rightIKTarget;
+    public Transform hintLeft, hintRight;
+    public float ikWeight = 1.0f;
+    #endregion
+
+    [Header("valores dinámicos de IK")]
+    public Vector3 leftFootPosition, rightFootPosition;
+    public Quaternion leftFootRotation, rightFootRotation;
+    public float leftFootWeight, rightFootWeight;
+
+    [Header("más valores dinámicos de IK")]
+    public float offsetY;
+    public float lookIkWeight = 1.0f;
+    public float bodyWEight = 0.7f;
+    public float headWEight = 0.9f;
+    public float eyesWEight = 1.0f;
+    public float clampWEight = 1.0f;
+
+    public Transform lookPosition;
+
+    #endif
+
+
+
+    void Start () {
+        animator = GetComponent<Animator>();
+
+        LeftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
+        rightFoot = animator.GetBoneTransform(HumanBodyBones.RightFoot);
+    }
+	
+	void Update () {
+		
+	}
+}
